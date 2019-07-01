8d2a78788afea04dc39d5798b149406f35cd3b80
Petschko <peter@petschko.org>
2019-07-01 18:10:23 +0200

    Fixed invalid Eclipse-State to exit the code... >.<

--- da-link-fixer.user.js
+++ da-link-fixer.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://petschko.org/deviantart/link-fixer
 // @description  Fixes the Links in the old Version, which the Eclipse-Team has broken...
 // @author       Peter Dragicevic [peter@petschko.org]
-// @version      0.1.0
+// @version      1.0.0
 // @encoding     utf-8
 // @homepage     https://petschko.org/
 // @homepage     https://github.com/Petschko/DA-Link-Fixer
@@ -129,7 +129,7 @@
 	 */
 	function init() {
 		// Exit if the user is on Eclipse
-		if(! isEclipseEnabled())
+		if(isEclipseEnabled())
 			return;
 
 		// Fix the poor links from the old version...
