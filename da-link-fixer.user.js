517e2501543edf7bd234a22144f5ae075a83d68a
Petschko <peter@petschko.org>
2019-07-01 16:42:24 +0200

    Initial commit

---
+++ da-link-fixer.user.js
@@ -0,0 +1,136 @@
+// ==UserScript==
+// @name         Deviantart-Link fixer
+// @namespace    https://petschko.org/deviantart/link-fixer
+// @description  Fixes the Links in the old Version, which the Eclipse-Team has broken...
+// @author       Peter Dragicevic [peter@petschko.org]
+// @version      0.0.1
+// @encoding     utf-8
+// @homepage     https://petschko.org/
+// @homepage     https://petschko.org/
+// @licence      https://creativecommons.org/licenses/by-nc-sa/3.0/
+// @include      http://deviantart.com/*
+// @include      https://deviantart.com/*
+// @include      http://*.deviantart.com/*
+// @include      https://*.deviantart.com/*
+// @grant        unsafeWindow
+// @grant        GM_getResourceText
+// @grant        GM_getResourceURL
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	const scriptName = 'Deviantart-Link-Fixer';
+	const debug = true; // Turns on all kind of messages, rather annoying for daily use, mostly for development, keep it false
+
+	/**
+	 * Checks if the user is currently in Eclipse
+	 *
+	 * @returns {boolean} - User is in Eclipse
+	 */
+	function isEclipseEnabled() {
+		let switchToEclipse = document.getElementById('oh-menu-eclipse-toggle');
+
+		return ! switchToEclipse;
+	}
+
+	/**
+	 * Cleans the Link from all that junk
+	 *
+	 * Also helps to improve data-privacy^^
+	 *
+	 * @param {Element} link - The link which should be purified
+	 */
+	function cleanupLink(link) {
+		if(link.hasAttribute('ping'))
+			link.removeAttribute('ping');
+
+		if(link.hasAttribute('data-sigil'))
+			link.removeAttribute('data-sigil');
+
+		if(link.hasAttribute('data-meta'))
+			link.removeAttribute('data-meta');
+
+		if(link.hasAttribute('target'))
+			link.removeAttribute('target');
+
+		if(link.hasAttribute('data-ga_click_event'))
+			link.removeAttribute('data-ga_click_event');
+
+		// Download-Button junk (not needed just javascript junk and for tracking also cause the download button to bug)
+		if(link.hasAttribute('data-gmiclass'))
+			link.removeAttribute('data-gmiclass');
+
+		if(link.hasAttribute('data-download_width'))
+			link.removeAttribute('data-download_width');
+
+		if(link.hasAttribute('data-download_height'))
+			link.removeAttribute('data-download_height');
+
+		if(link.hasAttribute('data-download_url'))
+			link.removeAttribute('data-download_url');
+
+		if(link.hasAttribute('data-dwait-domready'))
+			link.removeAttribute('data-dwait-domready');
+
+		if(link.hasAttribute('data-dwait-click'))
+			link.removeAttribute('data-dwait-click');
+
+		if(link.hasAttribute('data-dwait-deps'))
+			link.removeAttribute('data-dwait-deps');
+
+		if(link.hasAttribute('data-deviationid'))
+			link.removeAttribute('data-deviationid');
+
+		if(link.hasAttribute('gmindex'))
+			link.removeAttribute('gmindex');
+	}
+
+	/**
+	 * Fixes broken User-Links
+	 */
+	function fixUserLinks() {
+		let userLinksWrap = document.getElementsByClassName('username-with-symbol');
+
+		for(let i = 0; i < userLinksWrap.length; i++) {
+			let a = userLinksWrap[i].getElementsByClassName('username');
+
+			for(let n = 0; n < a.length; n++) {
+				// Remove crap from the link (some of them cause a prevent-default and a not found function)
+				cleanupLink(a[n]);
+
+				if(debug)
+					console.info(scriptName + ': Removed junk from Userlink (User: ' + a[n].innerHTML + ')');
+			}
+		}
+	}
+
+	/**
+	 * Purify all DA-Links...
+	 */
+	function fixAllLinks() {
+		let a = document.getElementsByTagName('a');
+
+		for(let i = 0; i < a.length; i++) {
+			cleanupLink(a[i]);
+		}
+	}
+
+	/**
+	 * Runs start tasks on Page load
+	 */
+	function init() {
+		// Exit if the user is on Eclipse
+		if(! isEclipseEnabled())
+			return;
+
+		// Fix the poor links from the old version...
+		//fixUserLinks(); <-- in the end doesnt needed xD
+		fixAllLinks();
+	}
+
+	// Adds start EventListener
+	window[window.addEventListener ? 'addEventListener' : 'attachEvent'](
+		window.addEventListener ? 'load' : 'onload', init, false);
+})();
---
+++ readme.md
@@ -0,0 +1,44 @@
+# DeviantArt Link Fixer
+
+## What is this Project for?
+----
+With this Script you can open all link like a half year before.
+
+The last month the DeviantArt Team goes on more aggressive with the Eclipse-Project. In that way they even start to break the old Version without repair stuff.
+
+Lately the even broke a couple of links, which can now only opened with a little workaround (open in new tab), which is annoying. This script fixes ALL links on the page and removes junk from them, such as:
+
+- Google-Analytic values
+- Ping-Attribute
+- Other values which cause the link to be stuck _(most of them are only for track users anyway, so we dont need them)_
+- The target value (Often used for open in new tab)
+
+
+Often these Links are affected (broken) by the changes of the WIX-Team:
+
+- Download-"Buttons"
+- View-Gallery Links
+- Username Links
+
+## Installation
+----
+
+### Firefox
+1. Get a Script-Manager like [Tampermonkey](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/)
+2. [Install this Script](https://insertLinkHere)
+
+### Chrome
+1. Get [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo).
+2. [Install this Script](https://insertLinkHere)
+
+## Credits
+----
+[Angel-Tigercloud](https://www.deviantart.com/angel-tigercloud)
+
+## Contact
+----
+
+If you have questions or still find unusable links let me know, you can write me here:
+
+- The [Issue-Section](https://insertLinkHere) of this Project
+- E-Mail me: peter@petschko.org
